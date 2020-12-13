1dbf617fef1c42dc19aa39de4d651cf276530925
Grigoriy Kozheletov <misteryrus@gmail.com>
2020-12-13 19:39:01 +0300

    fix in index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,9 @@
 
     <div class="flex justify-between">
         <h2 class="text-2xl">Tasks list:</h2>
-        <a class="text-xl text-white py-2 px-8 rounded-lg bg-yellow-900" href="{{route('tasks.create')}}">Add task</a>
+        @auth
+            <a class="text-xl text-white py-2 px-8 rounded-lg bg-yellow-900" href="{{route('tasks.create')}}">Add task</a>
+        @endauth
     </div>
 
     @forelse($tasks as $index => $task)
@@ -12,14 +14,16 @@
             <div>{{++$index}}. {{$task->task}}</div>
             <ul class="flex flex-col md:flex-row">
                 <li class="ml-3 text-green-500"><a href="{{route('tasks.show',$task)}}">Show</a></li>
-                <li class="ml-3 text-yellow-500"><a href="{{route('tasks.edit',$task)}}">Edit</a></li>
-                <li class="ml-3 text-red-500">
-                    <form action="{{route('tasks.delete',$task)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button>Delete</button>
-                    </form>
-                </li>
+                @auth
+                    <li class="ml-3 text-yellow-500"><a href="{{route('tasks.edit',$task)}}">Edit</a></li>
+                    <li class="ml-3 text-red-500">
+                        <form action="{{route('tasks.delete',$task)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button>Delete</button>
+                        </form>
+                    </li>
+                @endauth
             </ul>
         </div>
     @empty
